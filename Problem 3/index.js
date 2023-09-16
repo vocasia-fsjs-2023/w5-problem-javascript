b017287d857baae655fd8b7104473a36b7be0f58
Leonardo Ephraim <142810059+leonardoephraim@users.noreply.github.com>
2023-09-16 22:19:07 +0700

    problem 3 done

--- Problem 3/index.js
+++ Problem 3/index.js
@@ -1,2 +1,31 @@
 var nama;
 var nilai;
+
+// Meminta pengajar memasukkan nama dan nilai mahasiswa secara manual
+nama = prompt("Masukkan nama siswa:");
+nilai = parseFloat(prompt("Masukkan nilai siswa (0-100):"));
+
+// Memeriksa apakah nilai mahasiswa berada dalam rentang yang valid
+if (nilai >= 0 && nilai <= 100) {
+  // Menggunakan struktur percabangan untuk menentukan deskripsi nilai
+  var deskripsiNilai;
+
+  if (nilai >= 80 && nilai <= 100) {
+    deskripsiNilai = "A";
+  } else if (nilai >= 65 && nilai < 80) {
+    deskripsiNilai = "B";
+  } else if (nilai >= 50 && nilai < 65) {
+    deskripsiNilai = "C";
+  } else if (nilai >= 35 && nilai < 50) {
+    deskripsiNilai = "D";
+  } else {
+    deskripsiNilai = "E";
+  }
+
+  // Menampilkan deskripsi nilai dan nama siswa
+  console.log(`Nama siswa: ${nama}`);
+  console.log(`Deskripsi nilai: ${deskripsiNilai}`);
+} else {
+  // Menampilkan pesan jika nilai tidak valid
+  console.log("Nilai Invalid");
+}
