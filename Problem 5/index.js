ce9fa1b44e4bb430bcb50422d07fde39d1ecae84
Leonardo Ephraim <142810059+leonardoephraim@users.noreply.github.com>
2023-09-16 23:05:13 +0700

    done problem 5

--- Problem 5/index.js
+++ Problem 5/index.js
@@ -47,16 +47,14 @@ var fifthWord4 = word4.substring(21, 25);
 
 var firstWordLength = exampleFirstWord4.length;
 // create new variables around here
-var secondWordLenght = secondWord4.length;
-var thirdWordLenght = thirdWord4.length;
-var fourthWordLenght = fourthWord.length;
-var fifthWordLenght = fifthWord.length;
+var secondWordLength = secondWord4.length;
+var thirdWordLength = thirdWord4.length;
+var fourthWordLength = fourthWord4.length;
+var fifthWordLength = fifthWord4.length;
 
 
-console.log(
-  "First Word: " + exampleFirstWord4 + ", with length: " + firstWordLength
-);
-console.log("Second Word: " + secondWord4 + ", with lenght: " + secondWordLenght);
-console.log("Third Word: " + thirdWord4 + ", with lenght: " + thirdWordLenght);
-console.log("Fourth Word: " + fourthWord4 + ", with lenght: " + fourthWordLenght);
-console.log("Fifth Word: " + fifthWord4 + ", with lenght: " + fifthWordLenght);
+console.log("First Word: " + exampleFirstWord4 + ", with length: " + firstWordLength);
+console.log("Second Word: " + secondWord4 + ", with length: " + secondWordLength);
+console.log("Third Word: " + thirdWord4 + ", with length: " + thirdWordLength);
+console.log("Fourth Word: " + fourthWord4 + ", with length: " + fourthWordLength);
+console.log("Fifth Word: " + fifthWord4 + ", with length: " + fifthWordLength);
