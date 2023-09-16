e57427bb116791a5ac3807aee46da724f597e5ac
Firda Ayu Hassanah <131138947+userfirdaayuuu@users.noreply.github.com>
2023-09-16 09:31:54 +0700

    Update index.js

--- Problem 3/index.js
+++ Problem 3/index.js
@@ -1,2 +1,28 @@
-var nama;
-var nilai;
+// Input nilai
+var nama = "Jae"; // nama siswa
+var nilai = 78; // nilai siswa
+
+
+// Inisialisasi variabel grade dengan nilai default
+var grade = "Nilai Invalid";
+
+// Cek nilai berada dalam rentang yang valid
+if (nilai >= 0 && nilai <= 100) {
+  // Memeriksa rentang nilai
+  if (nilai >= 80 && nilai <= 100) {
+    grade = "A";
+  } else if (nilai >= 65 && nilai <= 79) {
+    grade = "B";
+  } else if (nilai >= 50 && nilai <= 64) {
+    grade = "C";
+  } else if (nilai >= 35 && nilai <= 49) {
+    grade = "D";
+  } else {
+    grade = "E";
+  }
+}
+
+// Menampilkan hasil
+console.log("Nama : " + nama);
+console.log("Nilai : " + nilai);
+console.log("Grade : " + grade);
