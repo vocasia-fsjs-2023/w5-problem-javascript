c66b5dbefb4493329dcfa1928c04eb89dff9a11b
yunanda0050 <142362343+yunanda0050@users.noreply.github.com>
2023-09-17 09:43:35 +0700

    Problem 3

--- Problem 3/index.js
+++ Problem 3/index.js
@@ -1,2 +1,21 @@
-var nama;
-var nilai;
+var nama = 'seungmin';
+var nilai = '101';
+
+if (nilai >= 0 && nilai <= 34) {
+    console.log(nama + " mendapatkan nilai E");
+}
+else if (nilai >= 35 && nilai <= 49) {
+    console.log(nama + " mendapatkan nilai D");
+}
+else if (nilai >= 50 && nilai <= 64) {
+    console.log(nama + " mendapatkan nilai C");
+}
+else if (nilai >= 65 && nilai <= 79) {
+    console.log(nama + " mendapatkan nilai B");
+}
+else if (nilai >= 80 && nilai <= 100) {
+    console.log(nama + " mendapatkan nilai A");
+}
+else {
+    console.log("Nilai Invalid");
+}
