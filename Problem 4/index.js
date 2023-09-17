633b950ce2b42b3cd1062391fc77adefcdcc5e94
yunanda0050 <142362343+yunanda0050@users.noreply.github.com>
2023-09-17 09:12:47 +0700

    Problem 4

--- Problem 4/index.js
+++ Problem 4/index.js
@@ -1,5 +1,27 @@
-var hasil = "";
-// Kerjakan dibawah sini
+var hasil = '';
+var random = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
 
-// Jangan melewati sini
+switch (random) {
+    case 1: {
+      console.log("coba lagi ya");
+      break;
+    }
+    case 2: {
+      console.log("selamat kamu mendapatkan kupon sebanyak 5");
+      break;
+    }
+    case 3: {
+        console.log("selamat kamu mendapatkan kupon sebanyak 15");
+        break;
+      }
+      case 4: {
+        console.log("selamat kamu mendapatkan kupon sebanyak 50");
+        break;
+      }
+      case 5: {
+        console.log("WOW, kamu menang jackpot! Selamat!!");
+        break;
+      }
+}  
+
 console.log(hasil);
