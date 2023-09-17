6cdb2d64f41a2e08dba889a936a66733d1eea792
Izzuddin AlFatah <68839136+AlFatah743@users.noreply.github.com>
2023-09-17 11:24:04 +0700

    Problem 4 Week 5
    Izzuddin AlFatah

--- Problem 4/index.js
+++ Problem 4/index.js
@@ -1,5 +1,26 @@
-var hasil = "";
-// Kerjakan dibawah sini
+const angkaRandom = Math.floor(Math.random() * 5) + 1;
 
-// Jangan melewati sini
-console.log(hasil);
+let pesan = "";
+
+switch (angkaRandom) {
+  case 1:
+    pesan = "Coba lagi ya";
+    break;
+  case 2:
+    pesan = "Selamat kamu mendapatkan kupon sebanyak 5";
+    break;
+  case 3:
+    pesan = "Selamat kamu mendapatkan kupon sebanyak 15";
+    break;
+  case 4:
+    pesan = "Selamat kamu mendapatkan kupon sebanyak 50";
+    break;
+  case 5:
+    pesan = "WOW, kamu menang jackpot! Selamat!!";
+    break;
+  default:
+    pesan = "Angka acak di luar rentang 1-5";
+    break;
+}
+
+console.log(pesan);
