11a7728d8edd4b94676d5c87c954474a510e5acb
AndaraAdisti <142381605+AndaraAdisti@users.noreply.github.com>
2023-09-17 10:25:23 +0700

    problem6 done
    problem 6

--- Problem 6/index.js
+++ Problem 6/index.js
@@ -1,6 +1,26 @@
 var kata = "I love Javascript";
 
-// Do your work here
+// Membuat fungsi untuk memeriksa apakah sebuah karakter adalah huruf vokal
+function isVowel(char) {
+  return 'aeiouAEIOU'.indexOf(char) !== -1;
+}
 
-// Dont work after this line
+// Membuat variabel baru untuk menyimpan kata hasil penggantian
+var hasil = "";
+
+// Loop melalui setiap karakter dalam kata
+for (var i = 0; i < kata.length; i++) {
+  var char = kata[i];
+  
+  // Jika karakter adalah huruf vokal, ganti dengan "$", jika tidak, biarkan seperti itu
+  if (isVowel(char)) {
+    hasil += '$';
+  } else {
+    hasil += char;
+  }
+}
+
+// Menggantikan nilai kata dengan hasil yang sudah dimodifikasi
+kata = hasil;
+
 console.log(kata);
