6f807360891f3b366e2a4dedd9afb81a7389df4e
rifqiabdi <rifqiabdillah242@gmail.com>
2023-09-17 11:23:37 +0700

    Mengumpulkan tugas week 5

--- Problem 6/index.js
+++ Problem 6/index.js
@@ -1,6 +1,24 @@
 var kata = "I love Javascript";
 
 // Do your work here
-
+function gantiVokal(kata) {
+  let hasil = "";
+  for (let a = 0; a < 17; a++) {
+    let huruf = kata[a];
+    if (
+      huruf === "a" ||
+      huruf === "i" ||
+      huruf === "u" ||
+      huruf === "e" ||
+      huruf === "o"
+    ) {
+      hasil += "$";
+    } else {
+      hasil += huruf;
+    }
+  }
+  return hasil;
+}
+kata = gantiVokal(kata);
 // Dont work after this line
 console.log(kata);
