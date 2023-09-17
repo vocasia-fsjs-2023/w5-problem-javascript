6c087b1fdd48fb472d36e2ecbcd3e616d6238003
zulfaapril25 <142382739+zulfaapril25@users.noreply.github.com>
2023-09-17 08:34:06 +0700

    Problem 6 done
    Problem 6

--- Problem 6/index.js
+++ Problem 6/index.js
@@ -1,6 +1,20 @@
 var kata = "I love Javascript";
 
 // Do your work here
+var hasil = '';
+
+for (var i = 0; i < kata.length; i++) {
+    var huruf = kata[i];
+    
+    if (huruf === 'a' || huruf === 'i' || huruf === 'u' || huruf === 'e' || huruf === 'o' ||
+        huruf === 'A' || huruf === 'I' || huruf === 'U' || huruf === 'E' || huruf === 'O') {
+        hasil += '$';
+    } else {
+        hasil += huruf;
+    }
+}
+
+kata = hasil; 
 
 // Dont work after this line
 console.log(kata);
