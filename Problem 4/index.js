605bd564240517a7fe5fa415cfd2fa495d415891
AndaraAdisti <142381605+AndaraAdisti@users.noreply.github.com>
2023-09-17 10:23:52 +0700

    problem4 done
    problem 4

--- Problem 4/index.js
+++ Problem 4/index.js
@@ -1,5 +1,25 @@
 var hasil = "";
-// Kerjakan dibawah sini
+var randomAngka = Math.floor(Math.random() * 5) + 1; 
 
-// Jangan melewati sini
+switch (randomAngka) {
+  case 1:
+    hasil = "coba lagi ya";
+    break;
+  case 2:
+    hasil = "selamat kamu mendapatkan kupon sebanyak 5";
+    break;
+  case 3:
+    hasil = "selamat kamu mendapatkan kupon sebanyak 15";
+    break;
+  case 4:
+    hasil = "selamat kamu mendapatkan kupon sebanyak 50";
+    break;
+  case 5:
+    hasil = "WOW, kamu menang jackpot! Selamat!!";
+    break;
+  default:
+    hasil = "Angka tidak valid";
+    break;
+}
+
 console.log(hasil);
