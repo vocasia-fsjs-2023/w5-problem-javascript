9625b3be72d3b7dd3bc84df2215d67825c8c4bf4
ilbaihaqi <baehaqiilham0@gmail.com>
2023-09-17 08:44:55 +0700

    update problem 6, 7

--- Problem 6/index.js
+++ Problem 6/index.js
@@ -1,6 +1,20 @@
 var kata = "I love Javascript";
 
+
 // Do your work here
+var newKata = "";
+
+for (var i = 0; i < kata.length; i++) {
+  var huruf = kata[i];
+  if ((huruf === 'a' || huruf === 'e' || huruf === 'i' || huruf === 'o' || huruf === 'u' ||
+      huruf === 'A' || huruf === 'E' || huruf === 'I' || huruf === 'O' || huruf === 'U') && i !== 0) {
+        newKata += '$';
+  } else {
+    newKata += huruf;
+  }
+}
+
+kata = newKata;
 
 // Dont work after this line
 console.log(kata);
