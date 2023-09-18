93b3ea44ab64d23d6a76821e61a6e8a05e00af3c
Firda Ayu Hassanah <131138947+userfirdaayuuu@users.noreply.github.com>
2023-09-18 14:06:03 +0700

    Update problem 6

--- Problem 6/index.js
+++ Problem 6/index.js
@@ -2,5 +2,22 @@ var kata = "I love Javascript";
 
 // Do your work here
 
+var hasil = "";
+
+for (var i = 0; i < kata.length; i++) {
+  var karakter = kata[i];
+  if (karakter === 'a' || karakter === 'A' ||
+      karakter === 'e' || karakter === 'E' ||
+      karakter === 'i' || karakter === 'I' ||
+      karakter === 'o' || karakter === 'O' ||
+      karakter === 'u' || karakter === 'U') {
+    hasil += '$';
+  } else {
+    hasil += karakter;
+  };
+};
+
+var kata = hasil;
+
 // Dont work after this line
 console.log(kata);
