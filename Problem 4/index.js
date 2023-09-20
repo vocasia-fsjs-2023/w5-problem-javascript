b701efca4d7e14b7f2ad9cec9ca99c61acc8cb3e
syafril andrew nuruddin <53548443+syafril2204@users.noreply.github.com>
2023-09-20 17:51:22 +0700

    problem 4

--- Problem 4/index.js
+++ Problem 4/index.js
@@ -1,5 +1,32 @@
 var hasil = "";
 // Kerjakan dibawah sini
 
+var randomNumber = Math.floor(Math.random() * 5) + 1;
+
+
+
+// Use switch-case to determine the message based on the random number
+switch (randomNumber) {
+    case 1:
+        hasil = "coba lagi ya";
+        break;
+    case 2:
+        hasil = "selamat kamu mendapatkan kupon sebanyak 5";
+        break;
+    case 3:
+        hasil = "selamat kamu mendapatkan kupon sebanyak 15";
+        break;
+    case 4:
+        hasil = "selamat kamu mendapatkan kupon sebanyak 50";
+        break;
+    case 5:
+        hasil = "WOW, kamu menang jackpot! Selamat!!";
+        break;
+    default:
+        hasil = "Angka tidak valid";
+}
+
+
+
 // Jangan melewati sini
 console.log(hasil);
