25d560bec5fb14035c1a2a6e2d20b085e824147d
syafril andrew nuruddin <53548443+syafril2204@users.noreply.github.com>
2023-09-20 17:48:00 +0700

    problem 3

--- Problem 3/index.js
+++ Problem 3/index.js
@@ -1,2 +1,24 @@
-var nama;
-var nilai;
+var nama = "Syafril";  // Ganti dengan nama siswa yang Anda inginkan
+var nilai = 75;             // Ganti dengan nilai yang Anda inginkan
+
+// Periksa apakah nilai dalam rentang yang valid (0-100)
+if (nilai < 0 || nilai > 100) {
+    console.log("Nilai Invalid");
+} else {
+    // Tentukan deskripsi nilai berdasarkan rentang
+    var deskripsiNilai;
+    if (nilai >= 80 && nilai <= 100) {
+        deskripsiNilai = "A";
+    } else if (nilai >= 65 && nilai <= 79) {
+        deskripsiNilai = "B";
+    } else if (nilai >= 50 && nilai <= 64) {
+        deskripsiNilai = "C";
+    } else if (nilai >= 35 && nilai <= 49) {
+        deskripsiNilai = "D";
+    } else {
+        deskripsiNilai = "E";
+    }
+
+    // Tampilkan deskripsi nilai dan nama siswa
+    console.log("Nilai " + nama + ": " + deskripsiNilai);
+}
