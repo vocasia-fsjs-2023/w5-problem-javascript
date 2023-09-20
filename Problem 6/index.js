f9073cc2f291b5bba2472e384929d0465490de09
syafril andrew nuruddin <53548443+syafril2204@users.noreply.github.com>
2023-09-20 18:02:43 +0700

    Problem 6

--- Problem 6/index.js
+++ Problem 6/index.js
@@ -1,6 +1,13 @@
-var kata = "I love Javascript";
+var kata = 'i love javascript';
+var kataBaru = '';
 
-// Do your work here
+for (var i = 0; i < kata.length; i++) {
+    var huruf = kata[i];
+    if (huruf === 'a' || huruf === 'e' || huruf === 'i' || huruf === 'o' || huruf === 'u') {
+        kataBaru += '$';
+    } else {
+        kataBaru += huruf;
+    }
+}
 
-// Dont work after this line
-console.log(kata);
+console.log(kataBaru);
